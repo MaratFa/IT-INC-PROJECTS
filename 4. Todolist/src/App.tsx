128575d1bb0950b9f934bf60770395dd1d9e433f
Marat <80181316+MaratFa@users.noreply.github.com>
2023-03-29 21:53:45 +0300

    import TaskType from Todolist.tsx

--- 4. Todolist/src/App.tsx
+++ 4. Todolist/src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-import { Todolist } from './Todolist';
+import {TaskType, Todolist } from './Todolist';
 
 function App() {
     const forTrack1='What to learn1111';
     const forTrack2='What to learn2222';
     
-    const tasks1 = [
+    const tasks1: Array<TaskType> = [
         { id: 1, title: "HTML&CSS", isDone: true }, //0
         { id: 2, title: "JS", isDone: true },       //1
         { id: 3, title: "ReactJS", isDone: false }, //2
         { id: 4, title: "ReactJS", isDone: false }  //3
     ]
-    const tasks2 = [
+    const tasks2: Array<TaskType> = [
         { id: 1, title: "Hello world", isDone: true },
         { id: 2, title: "I am Happy", isDone: false },
         { id: 3, title: "Yo", isDone: false }
