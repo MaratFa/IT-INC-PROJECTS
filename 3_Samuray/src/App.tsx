a38a9de149540c3fd2316b08b1253345595b6975
Marat <80181316+MaratFa@users.noreply.github.com>
2023-04-02 22:38:37 +0300

    Update App.tsx

--- 3_Samuray/src/App.tsx
+++ 3_Samuray/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
+import Footer from './Footer';
 import Header from './Header';
 import Technologies from './Technologies';
-
 
 const App = () => {
     return (
             <div>
                 <Header />
                 <Technologies />
+                <Footer />
             </div>
     );
 }
