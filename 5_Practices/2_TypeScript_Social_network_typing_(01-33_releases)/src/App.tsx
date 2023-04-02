4bb0212e64cb79480aa44800bbd724bcb80e10e4
Marat <80181316+MaratFa@users.noreply.github.com>
2023-04-02 20:30:37 +0300

    Renewing 'App.tsx'

--- 5_Practices/2_TypeScript_Social_network_typing_(01-33_releases)/src/App.tsx
+++ 5_Practices/2_TypeScript_Social_network_typing_(01-33_releases)/src/App.tsx
@@ -1,12 +1,32 @@
 import * as React from "react";
 import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 const App = () => {
     return (
+        <BrowserRouter>
             <div className='App'>
                 APP HELLO 
-            </div> 
+                <Routes>
+                  <Route path='/hello/' element={<HelloMessage message={"Hello Friends"} />} />                
+                  <Route path='/bye' element={<ByeMessage message={'Bye Samurai'} />} />
+                </Routes>
+            </div>            
+         </ BrowserRouter>
     );
+}
+
+type MessageType = {
+    message: string
+}
+
+function HelloMessage(props: MessageType) {
+  debugger
+    return <h1>{props.message}</h1>    
+}
+
+const ByeMessage: React.FC<MessageType> = (props) => {
+return <h1>{props.message}</h1>
 }
 
 export default App; 
