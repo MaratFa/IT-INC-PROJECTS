b0929eb0b88b4c5dc25fbcb5f7e5995b6ac6a3b1
Marat <80181316+MaratFa@users.noreply.github.com>
2023-04-03 02:22:49 +0300

    Update App.tsx

--- 3_Samuray/src/App.tsx
+++ 3_Samuray/src/App.tsx
@@ -1,15 +1,49 @@
 import './App.css';
-import Footer from './Footer';
-import Header from './Header';
-import Technologies from './Technologies';
 
 const App = () => {
     return (
-            <div>
-                <Header />
-                <Technologies />
-                <Footer />
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://mediaprocessor.websimages.com/fit/1920x1920/vijaykantindia.webs.com/web.png' alt=''/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src='https://justcoolidea.ru/wp-content/uploads/2017/10/KEYS.jpg' alt=''/>
+                </div>
+                <div>                    
+                    Avatar + Description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
             </div>
+        </div>
     );
 }
 
