8995753721aa85ad911fc098391ce6cd13d948ca
Marat <80181316+MaratFa@users.noreply.github.com>
2023-04-02 22:23:18 +0300

    Update App.tsx

--- 3_Samuray/src/App.tsx
+++ 3_Samuray/src/App.tsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import './App.css';
-import Technologies from './Technologies';
 import Header from './Header';
+import Technologies from './Technologies';
+
 
 const App = () => {
     return (
-        <div>
-            <Header />
-            <Technologies />
-        </div>
+            <div>
+                <Header />
+                <Technologies />
+            </div>
     );
 }
 
