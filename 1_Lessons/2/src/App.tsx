3d443d94dbcfa0f8efd104128fc8e380b351ab07
Marat <80181316+MaratFa@users.noreply.github.com>
2023-04-03 23:00:13 +0300

    Update App.tsx

--- 1_Lessons/2/src/App.tsx
+++ 1_Lessons/2/src/App.tsx
@@ -4,24 +4,24 @@ import { useState } from 'react';
 
 function App() {
 
-    let tasks1 = [
+    let[tasks, setTasks] = useState([
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
         { id: 3, title: "ReactJS", isDone: false },
         { id: 4, title: "React API", isDone: false },
         { id: 5, title: "GraphQL", isDone: false }
-    ]
+    ])
 
     function removeTask(id: number) {
-        tasks1 = tasks1.filter(task => task.id !== id)
-        console.log(tasks1)
+        let filteredTasks = tasks.filter(task => task.id !== id)
+        setTasks(filteredTasks)
     }
 
     return (
         <div className="App">
             <Todolist 
                 title="What to learn"
-                tasks={tasks1}
+                tasks={tasks}
                 removeTask={removeTask}/>
         </div>
     );
