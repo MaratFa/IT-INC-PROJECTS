6c19407874be84602ecadd7d07a0916e4e999f64
Marat <80181316+MaratFa@users.noreply.github.com>
2023-04-06 01:20:27 +0300

    Adding Header.tsx component

---
+++ 3_Samuray/src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return(
+        <header className='header'>
+            <img src='https://mediaprocessor.websimages.com/fit/1920x1920/vijaykantindia.webs.com/web.png' alt=''/>
+        </header>
+    )
+}
+
+export default Header;
