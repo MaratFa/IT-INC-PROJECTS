1d2e43a478b0356c9a3d2b6d3d86563d7d1abb5e
Marat <80181316+MaratFa@users.noreply.github.com>
2023-04-06 01:21:47 +0300

    Adding Profile.tsx component

---
+++ 3_Samuray/src/components/Profile.tsx
@@ -0,0 +1,26 @@
+const Profile = () => {
+    return(
+        <div className='content'>
+        <div>
+            <img src='https://justcoolidea.ru/wp-content/uploads/2017/10/KEYS.jpg' alt=''/>
+        </div>
+        <div>                    
+            Avatar + Description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+        </div>
+        <div>
+            Post 1
+        </div>
+        <div>
+            Post 2
+        </div>
+    </div>
+    )
+}
+
+export default Profile;
