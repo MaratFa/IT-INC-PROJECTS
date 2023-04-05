7710783a74d0eff45e5d47bbeae1c7a3f3a25852
Marat <80181316+MaratFa@users.noreply.github.com>
2023-04-06 01:15:39 +0300

    Breakdown into components

--- 3_Samuray/src/App.tsx
+++ 3_Samuray/src/App.tsx
@@ -1,48 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 const App = () => {
     return (
-        <div className='app-wrapper'>
-            <header className='header'>
-                <img src='https://mediaprocessor.websimages.com/fit/1920x1920/vijaykantindia.webs.com/web.png' alt=''/>
-            </header>
-            <nav className='nav'>
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-            </nav>
-            <div className='content'>
-                <div>
-                    <img src='https://justcoolidea.ru/wp-content/uploads/2017/10/KEYS.jpg' alt=''/>
-                </div>
-                <div>                    
-                    Avatar + Description
-                </div>
-                <div>
-                    My posts
-                    <div>
-                        New post
-                    </div>
-                </div>
-                <div>
-                    Post 1
-                </div>
-                <div>
-                    Post 2
-                </div>
-            </div>
+        <div className='grid-container'>
+            <Header />
+            <Navbar />
+            <Profile />
         </div>
     );
 }
