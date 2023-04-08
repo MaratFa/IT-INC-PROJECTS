81566b28832568138d262576c44feca5f7d2d779
Marat <evolution99@rambler.ru>
2023-04-09 02:47:51 +0300

    deleting css components

--- 3_Samuray/src/components/Profile.tsx
+++ 3_Samuray/src/components/Profile.tsx
@@ -1,27 +1,29 @@
-import './Profile.css'
+import s from './Profile.module.css'
 
 const Profile = () => {
     return(
-        <div className='content'>
-        <div>
-            <img src='https://justcoolidea.ru/wp-content/uploads/2017/10/KEYS.jpg' alt=''/>
-        </div>
-        <div>                    
-            Avatar + Description
-        </div>
-        <div>
-            My posts
+        <div className={s.content}>
             <div>
-                New post
+                <img src='https://justcoolidea.ru/wp-content/uploads/2017/10/KEYS.jpg' alt=''/>
             </div>
+            <div>                    
+                Avatar + Description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div className={s.posts}>
+                    <div className={s.item}>             
+                        Post 1
+                    </div>
+                    <div className={s.item}>
+                        Post 2
+                    </div>
+                </div>
+            </div>        
         </div>
-        <div>
-            Post 1
-        </div>
-        <div>
-            Post 2
-        </div>
-    </div>
     )
 }
 
