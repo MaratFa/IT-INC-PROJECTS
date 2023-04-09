2a3df707418afe9fcd5a6bc2362a2f3ce4f7728a
Marat <evolution99@rambler.ru>
2023-04-09 05:02:47 +0300

    Added Navbar folder for Navbar components

--- 3_Samuray/src/App.tsx
+++ 3_Samuray/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile';
 
 const App = () => {
