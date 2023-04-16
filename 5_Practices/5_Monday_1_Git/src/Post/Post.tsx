379fc4d3ee082c00518082cb3b71e5e1ae9c6934
Marat <evolution99@rambler.ru>
2023-04-16 04:33:45 +0300

    Modified files

--- 5_Practices/5_Monday_1_Git/src/Post/Post.tsx
+++ 5_Practices/5_Monday_1_Git/src/Post/Post.tsx
@@ -2,6 +2,15 @@ export const Post = () => {
     return (
         <div>
             <h2>Posts4</h2>
+            <h2>Posts4</h2>
+            <h2>Posts4</h2>
+            <h2>Posts4</h2>
+            <h2>Posts4</h2>
+            <h2>Posts4</h2>
+            <h2>Posts4</h2>
+            <h2>Posts4</h2>
+            <h2>Posts4</h2>
+            <h2>Posts4</h2>
         </div>
     )
 }
--- 5_Practices/5_Monday_1_Git/src/User/User.tsx
+++ 5_Practices/5_Monday_1_Git/src/User/User.tsx
@@ -2,6 +2,13 @@ export const User = () => {
     return (
         <div>
             <h2>User2</h2>
+            <h2>User2</h2>
+            <h2>User2</h2>
+            <h2>User2</h2>
+            <h2>User2</h2>
+            <h2>User2</h2>
+            <h2>User2</h2>
+            <h2>User2</h2>
         </div>
     )
 }
